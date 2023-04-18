4359bfe7fd1af8535daee6d18935b8c34e6b8ad9
Shorlo <shorlo.sbg@gmail.com>
2023-04-18 16:40:31 +0200

    define Album model

--- models/Album.js
+++ models/Album.js
@@ -21,3 +21,37 @@
 |   with this program. If not, see <http://www.gnu.org/licenses/>.              |
 |                                                                               |
 '==============================================================================*/
+
+const {Schema, model} = require('mongoose');
+
+const AlbumSchema = Schema
+({
+     artist:
+     {
+          type: Schema.ObjectId,
+          ref: 'Artist'
+     },
+     title:
+     {
+          type: String,
+          required: true
+     },
+     description: String,
+     year:
+     {
+          type: Number,
+          required: true
+     },
+     image:
+     {
+          type: String,
+          default: 'default.png'
+     },
+     created_at:
+     {
+          type: Date,
+          default: Date.now
+     }
+});
+
+module.exports = model('Album', AlbumSchema, 'Albums');
